11dd58ecf8ae8442a5f13664ade70bc2855b1bf9
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-09-27 20:03:39 -0300

    feat: created python set up script in database for heart-failure models(kmean, kmedoid, mlp)

--- MLMODELS/heart-failure/setmodels.py
+++ MLMODELS/heart-failure/setmodels.py
@@ -1,26 +1,57 @@
 import pymongo
 from bson.binary import Binary
 from datetime import datetime
-import pickle
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client['galeno_database']
 models_collection = db['models']
 
+with open('heart-failure-mlp.pkl', 'rb') as f:
+    heart_failure_mlp_binary = f.read()
+
+with open('heart-failure-kmean.pkl', 'rb') as f:
+    heart_failure_kmean_binary = f.read()
 
 with open('heart-failure-mlp.pkl', 'rb') as f:
-    model_binary = f.read()
+    heart_failure_kmedoid_binary = f.read()
 
 
-model_document = {
+heart_failure_mlp_document = {
     "name": "heart-failure-mlp",
     "disease": "heart failure",
     "type": "mlp",
     "last_update": datetime.now(),
-    "input_description": "15 numbers within 0,1",
+    "input_description": ', '.join(["Age","ChestPainType", "Cholesterol", "ExerciseAngina", "FastingBS",  "MaxHR", "Oldpeak","RestingBP",	"RestingECG", "Sex","ST_Slope"]),
     "output_description": "0 = negative to heart failure, 1 = positive to heart failure",
-    "model": Binary(model_binary)
+    "model": Binary(heart_failure_mlp_binary)
 }
 
-result = models_collection.insert_one(model_document)
+heart_failure_kmean_document = {
+    "name": "heart-failure-kmean",
+    "disease": "heart failure",
+    "type": "kmean",
+    "last_update": datetime.now(),
+    "input_description": ', '.join(["Age","ChestPainType", "Cholesterol", "ExerciseAngina", "FastingBS",  "MaxHR", "Oldpeak","RestingBP",	"RestingECG", "Sex","ST_Slope"]),
+    "output_description": "0 = high probability of positive to heart failure , 1 = inconclusive",
+    "model": Binary(heart_failure_kmean_binary)
+}
+
+heart_failure_kmedoid_document = {
+    "name": "heart-failure-kmedoid",
+    "disease": "heart failure",
+    "type": "kmedoid",
+    "last_update": datetime.now(),
+    "input_description": ', '.join(["Age","ChestPainType", "Cholesterol", "ExerciseAngina", "FastingBS",  "MaxHR", "Oldpeak","RestingBP",	"RestingECG", "Sex","ST_Slope"]),
+    "output_description": "0 = high probability of positive to heart failure , 1 = inconclusive",
+    "model": Binary(heart_failure_mlp_binary)
+}
+
+result = models_collection.insert_one( heart_failure_mlp_document)
 print(f"Model inserted with _id: {result.inserted_id}")
+
+result = models_collection.insert_one( heart_failure_kmean_document)
+print(f"Model inserted with _id: {result.inserted_id}")
+
+result = models_collection.insert_one( heart_failure_kmedoid_document)
+print(f"Model inserted with _id: {result.inserted_id}")
+
