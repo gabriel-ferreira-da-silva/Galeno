8e490e703a1a2f5f1e28df849ad62f4ae20a084d
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-10-17 22:43:44 -0300

    deleted unused utils files from backend

--- backend/utils/commom_utils.py
+++
@@ -1,2 +0,0 @@
-def getInput(data):
-    return data['input_data']
--- backend/utils/lung_cancer_utils.py
+++
@@ -1,5 +0,0 @@
-from .database_utils import *
-
-def load_lung_cancer_mlp():
-    lung_cancer_mlp = load_model("lung-cancer-mlp")
-    return lung_cancer_mlp
