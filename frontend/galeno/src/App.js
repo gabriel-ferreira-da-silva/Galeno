660d7dbf4f39e0b203ae8ed28c229c844e4ae003
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-10-28 13:48:05 -0300

    added collapseable sidebar menu

--- frontend/galeno/src/App.js
+++ frontend/galeno/src/App.js
@@ -5,18 +5,25 @@ import AddModelPage from './pages/AddModelPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Galnavbar } from './components/navbar/Galnavbar';
 import TrainModelPage from './pages/TrainModelPage';
+import { Sidebar } from 'react-pro-sidebar';
+import SideMenu from './components/Sidemenu/SideMenu';
+import style from './style.module.css'
+
 function App() {
   return (
     <div>
       <Router>
         <Galnavbar></Galnavbar>
-        <Routes>
-        <Route path="/" element={<FormPage/>}/>
-        <Route path="/predict" element={<FormPage/>}/>
-        <Route path="/addmodel" element={<AddModelPage/>}/>
-        <Route path="/train" element={<TrainModelPage/>}/>
-        <Route path="/info" element={<InfoPage/>}/>
-        </Routes>
+        <div className={style.body} style={{heigh:'100%'}}>
+          <SideMenu></SideMenu>
+          <Routes>
+            <Route path="/" element={<FormPage/>}/>
+            <Route path="/predict" element={<FormPage/>}/>
+            <Route path="/addmodel" element={<AddModelPage/>}/>
+            <Route path="/train" element={<TrainModelPage/>}/>
+            <Route path="/info" element={<InfoPage/>}/>
+          </Routes>
+        </div>
       </Router>
     </div>
 
---
+++ frontend/galeno/src/components/Sidemenu/SideMenu.js
@@ -0,0 +1,123 @@
+import React from 'react'; 
+import { Link } from "react-router-dom"; // Import Link for navigation
+
+import { Home, Folder, Message } from '@mui/icons-material';
+import style from './style.module.css'
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+
+import { useState } from 'react';
+
+function SideMenu() { 
+	const [collapsed, setCollapsed] = useState(false);
+
+
+	const handleToggleSidebar = () => {
+	  setCollapsed(!collapsed);
+	};
+  
+  
+	return (
+  
+	  <Sidebar collapsed={collapsed} className={style.sidebarpanel}>
+		<div onClick={handleToggleSidebar} className={style.togglebutton}>
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
+				<path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
+			</svg>
+		</div> 
+		<Menu className={style.menupanel}>
+			<Link to="/predict" className={style.linkholder}>
+				<MenuItem className={style.menuoption}>
+					
+						
+						<div>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calculator-fill" viewBox="0 0 16 16">
+							<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm2 .5v2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5m0 4v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5M4.5 9a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM4 12.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5M7.5 6a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM7 9.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5m.5 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM10 6.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5m.5 2.5a.5.5 0 0 0-.5.5v4a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 0-.5-.5z"/>
+						</svg>
+						{!collapsed && "predict"}
+						</div>
+						
+				</MenuItem>
+			</Link>
+
+			<Link to="/train"  className={style.linkholder}>
+				<MenuItem className={style.menuoption}>
+                    
+                    <div>
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-wrench-adjustable-circle-fill" viewBox="0 0 16 16">
+                        <path d="M6.705 8.139a.25.25 0 0 0-.288-.376l-1.5.5.159.474.808-.27-.595.894a.25.25 0 0 0 .287.376l.808-.27-.595.894a.25.25 0 0 0 .287.376l1.5-.5-.159-.474-.808.27.596-.894a.25.25 0 0 0-.288-.376l-.808.27z"/>
+                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m-6.202-4.751 1.988-1.657a4.5 4.5 0 0 1 7.537-4.623L7.497 6.5l1 2.5 1.333 3.11c-.56.251-1.18.39-1.833.39a4.5 4.5 0 0 1-1.592-.29L4.747 14.2a7.03 7.03 0 0 1-2.949-2.951M12.496 8a4.5 4.5 0 0 1-1.703 3.526L9.497 8.5l2.959-1.11q.04.3.04.61"/>
+                    </svg>
+						{!collapsed && "train"}
+						
+						</div>
+
+				</MenuItem>
+            </Link>
+
+			<Link to="/adddisease"  className={style.linkholder}>
+				<MenuItem className={style.menuoption}>
+					
+				
+					<div>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-heart-fill" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M6.5 0A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0zm3 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z"/>
+							<path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1A2.5 2.5 0 0 1 9.5 5h-3A2.5 2.5 0 0 1 4 2.5zm4 5.982c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018"/>
+						</svg>
+					
+						{!collapsed && "add disease"}
+					
+					</div>
+
+				</MenuItem>
+            </Link>
+
+			<Link to="/addmodel"  className={style.linkholder}>
+				<MenuItem className={style.menuoption}>
+					
+					<div>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
+						<path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3q0-.405-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708M3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z"/>
+						</svg>
+						{!collapsed && "add model"}
+						</div>
+				</MenuItem>
+            </Link>
+
+			<Link to="/managemodels"  className={style.linkholder}>
+				<MenuItem className={style.menuoption}>
+					
+				
+					<div>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ui-checks-grid" viewBox="0 0 16 16">
+					<path d="M2 10h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1m9-9h3a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1m0 9a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zm0-10a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM2 9a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2zm7 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zM0 2a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5.354.854a.5.5 0 1 0-.708-.708L3 3.793l-.646-.647a.5.5 0 1 0-.708.708l1 1a.5.5 0 0 0 .708 0z"/>
+					</svg>
+					{!collapsed && "manage models"}
+					</div>
+
+				</MenuItem>
+            </Link>
+
+
+			<Link to="/info"  className={style.linkholder}>
+				<MenuItem className={style.menuoption}>
+
+                    
+                    <div>
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-question-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247m2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/>
+                    </svg>
+					{!collapsed && "about"}
+					</div>
+
+				</MenuItem>
+			</Link>
+
+		</Menu>
+  
+		 
+	  </Sidebar>
+  
+	);
+} 
+
+export default SideMenu;
